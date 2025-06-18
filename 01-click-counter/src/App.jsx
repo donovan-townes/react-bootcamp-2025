507cab937293d8b204f0e8b709826317d8f75daa
Donovan Townes <z8phyr@hotmail.com>
2025-06-18 12:19:00 -0500

    feat: basic click counter

--- 01-click-counter/src/App.jsx
+++ 01-click-counter/src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const [count, setCount] = useState(0)
+const MAX = 10
+const increment = () => {
+  
+  if (count+1 > 10) {
+    return
+  } else {
+    setCount(count+1)
+  }
+  return count
+}
+
+function resetCount() {
+  setCount(0)
+}
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={increment}>Count: {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <br />
+        <button onClick={resetCount} >Reset</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
