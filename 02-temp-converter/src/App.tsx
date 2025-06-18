a765995ffe0f6c9e151c0ed288da33c3bd8714b7
Donovan Townes <z8phyr@hotmail.com>
2025-06-18 16:21:40 -0500

    refactor, testing

--- 02-temp-converter/src/App.tsx
+++ 02-temp-converter/src/App.tsx
@@ -1,41 +1,65 @@
 import { useState } from 'react'
 import './App.css'
 
-function TempConverter({initial = 0}) {
-  const [celsius, setCelscius] = useState<number | "">(initial)
+  function TempUnit({unit}) {
+    return <input
+      type="number"
+      className = {hasError ? "error" : ""}
+      value= {unit === "Celsius" ?  celsius : unit === "Kelvin" ? kelvin : fahrenheit}
+      onChange={(e) => handleTempChange(e,unit)}
+
+          />
+  }
+
+  function TempConverter({initial = 0}) {
+  const [hasError, setHasError] = useState(false)
+  const [celsius, setCelsius] = useState<number | "">(initial)
   const fahrenheit = celsius === "" ? "" : (celsius * 9) / 5 + 32
+  const kelvin = celsius === "" ? "" : (celsius + 273.15)
 
 
-  const handleCChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const input = e.target.value
-    setCelscius(input === "" ? "" : Number(input))
+
+  function handleTempChange(e, unit) {
+    
+    const rawInput = e.target.value
+    if (rawInput === "") {
+        setCelsius("")
+        setHasError(false)
+        return
+      }     
+      const num = Number(rawInput)
+      const bad = isNaN(num)
+      
+      setHasError(bad)
+      console.log(rawInput)
+
+     switch(unit) {
+      case "Celsius": 
+        setCelsius(num)
+        break
+      case "Fahrenheit":
+        setCelsius(((num - 32) * 5) / 9)
+        break  
+      case "Kelvin":
+        setCelsius(num - 273.15)
+        break
+      default:
+          setCelsius(0)
+        }
+     
   }
-
-  const handleFChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const input = e.target.value
-    setCelscius( input === "" ? "" : ((Number(input) - 32) * 5) / 9)
-  }
-
-
+ 
   return (
     <>
      <h2>Temperature Converter</h2>
-    <div className="temp-box">
-    <h2>F</h2>
-    <input 
-      type="string" 
-      value={fahrenheit} 
-      onChange={handleFChange} 
-       />
-    </div>
-    <div>
+    <div className="input-boxes">
     <h2>C</h2>
-     <input 
-        type="string" 
-        value={celsius} 
-        onChange={handleCChange}
-        />
-    </div>
+    <TempUnit unit="Celsius"/>
+    <h2>F</h2>
+    <TempUnit unit="Fahrenheit"/>
+    <h2>K</h2>
+    <TempUnit unit="Kelvin"/>
+  </div>
 
     </>
   )
